63caa73da5481d1975542c4db6b9a8801446bb2d
albarodriguezi <alba.rodriguez.i@opendeusto.es>
2025-11-27 12:59:13 +0100

    Refactor UsageRepository to use Registry entity

--- src/main/java/es/deusto/sd/ecoembes/dao/UsageRepository.java
+++ src/main/java/es/deusto/sd/ecoembes/dao/UsageRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import es.deusto.sd.ecoembes.entity.UsageId;
-import es.deusto.sd.ecoembes.entity.Usage;
+import es.deusto.sd.ecoembes.entity.Registry;
 import es.deusto.sd.ecoembes.entity.Dumpster;
 
 @Repository
-public interface UsageRepository extends JpaRepository<Usage, UsageId>{
-	List<Usage> findByIdDate(LocalDate date);
-	List<Usage> findByIdDumpster(Dumpster dumpster);
-	Usage findByIdDateAndIdDumpster(LocalDate date, Dumpster dumpster);
+public interface UsageRepository extends JpaRepository<Registry, UsageId>{
+	List<Registry> findByIdDate(LocalDate date);
+	List<Regisytry> findByIdDumpster(Dumpster dumpster);
+	Registry findByIdDateAndIdDumpster(LocalDate date, Dumpster dumpster);
 }
+
